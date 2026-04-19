02a4c1bccc2161bd3a63c9143080fac07742bb5b
Leighna <23wh1a0531@bvrithyderabad.edu.in>
2026-04-19 11:37:18 +0000

    Add home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>College Fest Management System</h1>
+      <p>Welcome to the College Fest Management Platform</p>
+    </div>
+  );
+};
+
+export default Home;
